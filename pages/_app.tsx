b500fe29d9295b18ed3ea28891c1630a3a03004a
Scott Susanto <susantoscott@gmail.com>
2023-08-01 03:58:01 -0700

    Add standard metatags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const favicon =
+    "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🚀</text></svg>";
+
+  const meta = {
+    title: '',
+    description: '',
+    image: favicon,
+    type: 'website',
+  };
+
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+        <link rel="icon" href={favicon} />
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
